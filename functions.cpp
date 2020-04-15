c79fd76f4f043561a474737809fb8a4917a69855
Khanjawadkhan <63664842+Khanjawadkhan@users.noreply.github.com>
2020-04-15 21:19:28 +0500

    updating file

---
+++ functions.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+/*void kionaam()
+{
+	cout <<"hello"<<endl ;
+	cout <<"world"<<endl;
+	cout <<"Hi jawad khan"<<endl;
+}
+int main()
+{
+	for (int i=1;i<=10;i++)
+	{
+	kionaam();
+	}
+	system ("pause");
+	return 0;
+}*/
+/*void kionaam()
+{
+	bool a=true;
+	if(a)
+	{
+		cout <<"a is true" <<endl;
+	}
+}
+int main()
+{
+	kionaam();
+	kionaam();
+	system ("pause");
+	return 0;
+}*/
+/*#include<iostream>
+using namespace std;
+void kionaam();
+int main()
+{
+	kionaam();
+	system ("pause");
+	return 0;
+}
+void kionaam()
+{
+	bool a=true;
+	if(a)
+	{
+		cout <<"a is true" <<endl;
+	}
+}*/
+/*#include<iostream>
+using namespace std;
+int sum(int a,int b)
+{
+	return a+b;
+}
+int main()
+{
+	int r=sum(sum(2,2),sum(2,2));
+	cout << r;
+	system ("pause");
+	return 0;
+}*/
+/*#include<iostream>
+using namespace std;
+int sumi(int a,int b)
+{
+	return a+b;
+}
+int sumf (float a,float b)
+{
+	return a+b;
+}
+int main()
+{
+	int i;
+	float rf;
+	int r=sumf(2.5,2.5);
+		cout << r ;
+	system ("pause");
+	return 0;
+}
